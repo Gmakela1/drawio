aef970261ba026f8e89755f38073308694ae0a3b
Gmakela1 <gmakela1@github.com>
2026-07-19 18:52:08 -0500

    fix: cleaner SSE auto-refresh on index.html + editor page with Draw.loadPlugin capture

--- server/server.js
+++ server/server.js
@@ -21,6 +21,33 @@ app.get('/editor', (req, res) => {
 const apiRoutes = require('./routes/index');
 app.use('/api', apiRoutes);
 
+// Inject SSE auto-refresh script into drawio HTML pages
+app.use((req, res, next) => {
+    const originalSend = res.send;
+    res.send = function(body) {
+        if (typeof body === 'string' && res.get('Content-Type')?.includes('text/html')) {
+            const sseScript = `
+<script>
+(function() {
+    var evtSource = new EventSource('/api/watch');
+    evtSource.onmessage = function(e) {
+        try {
+            var data = JSON.parse(e.data);
+            if (data.event === 'reload') {
+                console.log('Diagram changed: ' + data.file + ' - reloading...');
+                window.location.reload();
+            }
+        } catch(ex) {}
+    };
+})();
+</script>`;
+            body = body.replace('</body>', sseScript + '</body>');
+        }
+        return originalSend.call(this, body);
+    };
+    next();
+});
+
 app.listen(PORT, () => {
     console.log(`drawio AI server running at http://localhost:${PORT}`);
     console.log(`API available at http://localhost:${PORT}/api`);
